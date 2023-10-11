201227f0a78b5e8c5026d28c110e10319037231c
Ranulph <ranulph@mailfence.run>
2023-10-12 00:07:56 +0300

    Initial Commit. Operational.

---
+++ src/index.ts
@@ -0,0 +1,39 @@
+
+export interface Env {
+	CURRENCIES: KVNamespace;
+	APP_ID: string;
+};
+
+export default {
+	async scheduled(event: ScheduledEvent, env: Env, ctx: ExecutionContext) {
+
+		const fetchProcessCurrencies = async () => {
+			const currencies: currencyRateList = await fetch('https://openexchangerates.org/api/latest.json?app_id=' + env.APP_ID).then(res => res.json());
+			const timestamp = currencies.timestamp;
+			const rates = currencies.rates;
+			for (const [currency, rate] of Object.entries(rates)) {
+				let current = await env.CURRENCIES.get(currency);
+				if (current) {
+					let currentJSON = await JSON.parse(current)
+					let updated = {
+						...currentJSON,
+						rateUSD: rate,
+						updatedAt: timestamp
+					};
+					env.CURRENCIES.put(currency, JSON.stringify(updated))
+				}
+			  }
+		}
+		// Runs 12:00AM every day.
+		ctx.waitUntil(fetchProcessCurrencies());
+	},
+  };
+
+type currencyRateList = {
+	disclaimer: string;
+	license: string;
+	timestamp: number;
+	base: string;
+	rates: object;
+};
+
